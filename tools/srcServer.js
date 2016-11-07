ddf91de2269e1c7130328d223b864a5edc5d0242
Chris Giglio <chris.giglio1@gmail.com>
2016-11-06 22:56:21 -0500

    API calls now functioning for searching chapters, adding/deleting a student, and adding a course.

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -6,9 +6,21 @@ import open from 'open';
 
 /* eslint-disable no-console */
 
-const port = 3000;
 const app = express();
 const compiler = webpack(config);
+
+var Database = require('./Database.js');
+var bodyParser = require('body-parser');
+// var Auth = require('Auth')
+
+/******************************************************************************
+Server Setup
+*******************************************************************************/
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
@@ -17,11 +29,82 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
+ // to support JSON-encoded bodies
+app.use(bodyParser.json());
+
+// to support URL-encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+
 app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
 
-var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+/******************************************************************************
+Routes
+*******************************************************************************/
+
+//login code
+app.get('/login', function(req, res) {
+  var userId = req.body.userId;
+  var pw = req.body.pw;
+
+  // Auth.login should try login, send either the correct user data
+  // or a notification of login failure
+  // res = Auth.login(userId, pw);
 });
+
+app.post('/signup', function(req, res) {
+	var name = req.body.name;
+	var pw = req.body.pw;
+	var email = req.body.email;
+	var exp_date = '2017-12-12';
+
+	// Auth.creatAccount should try to create account and return either
+	// creation verification or creation error
+	// Auth.createAccount(name, pw, email);
+	Database.addStudent(email, name, pw, exp_date, function(err, data) {
+		if (err) throw Error(err);
+		res.end(data);
+	});
+});
+
+app.post('/deleteStudent', function(req, res) {
+	var email = req.body.email;
+
+	Database.deleteStudent(email, function(err, data) {
+		if (err) throw Error(err);
+		res.end(data);
+	})
+});
+
+app.get('/student', function(req, res) {
+	// Auth.verify(req.email);
+	Database.getClasses(req.body.email, function(err, data) {
+		if (err) throw Error(err);
+		res.send(data);
+	});
+});
+
+app.post('/search', function(req, res) {
+	// Auth.verify(req.email);
+	Database.searchChapters(req.body.searchQuery, function(err, data) {
+		if (err) throw(err);
+		res.end(data);
+	});
+});
+
+app.post('/addCourse', function(req, res) {
+	// Auth.verify(req.email);
+	Database.addCourse(req.body.email, req.body.courseName, function(err, data) {
+		if (err) throw(err);
+		res.end(data);
+	});
+});
+
+app.post('/student/searchShade', function(req, res) {
+	// Auth.verify(req.userId);
+	data = Database.shadeSearch(req.searchQuery);
+	res.send(data);
+})
