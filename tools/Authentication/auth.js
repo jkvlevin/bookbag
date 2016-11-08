cd7419dbdbac58727bc91495a3e9ae8cf548bdd1
Joe Salter <jcsalter@princeton.edu>
2016-11-07 22:48:54 -0500

    Password hash and validation, JWT generateToken method added to Auth.js

---
+++ tools/Authentication/auth.js
@@ -0,0 +1,23 @@
+/******************************************************************************
+Module to Manage Auth
+*******************************************************************************/
+
+var bcrypt  = require('bcrypt-nodejs');
+var jwt     = require('jsonwebtoken')
+var config  = require('./config')
+
+// Return a hashed password
+exports.hashPassword = function(password) {
+	return bcrypt.hashSync(password);
+}
+
+// Validate password given attempted unhashed password from request and correct hashed password
+exports.validatePassword = function(attemptedPassword, hashedPassword) {
+	return bcrypt.compareSync(attemptedPassword, hashedPassword);
+}
+
+exports.generateToken = function(user) {
+	return jwt.sign(user, config.secret, {
+		expiresIn: 3600 // in seconds
+	});
+}
