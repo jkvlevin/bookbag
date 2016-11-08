8eca7d214f76c1ff9357df2254c152c966e08afd
Chris Giglio <chris.giglio1@gmail.com>
2016-11-07 22:27:03 -0500

    merging login

--- src/containers/StudentHome/index.js
+++ src/containers/StudentHome/index.js
@@ -34,29 +34,27 @@ class StudentHome extends React.Component {
       <div className="student-container">
           <Header showSearch hasUser/>
           <h2 style={{marginTop:"80px", marginLeft:"40px"}}> Welcome, Jake </h2>
-          <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={(i,e)=>this.handleSelect(i,e)}>
-            <Carousel.Item>
-              <img width={200} height={200} alt="200x200" src="https://pixabay.com/static/uploads/photo/2014/03/29/09/17/cat-300572_960_720.jpg"/>
-              <img width={200} height={200} alt="200x200" src="https://pixabay.com/static/uploads/photo/2014/03/29/09/17/cat-300572_960_720.jpg"/>
-              <img width={200} height={200} alt="200x200" src="https://pixabay.com/static/uploads/photo/2014/03/29/09/17/cat-300572_960_720.jpg"/>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img width={200} height={200} alt="200x200" src="http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg"/>
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img width={200} height={200} alt="200x200" src="https://pixabay.com/static/uploads/photo/2014/03/29/09/17/cat-300572_960_720.jpg"/>
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
+          <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={(i,e)=>this.handleSelect(i,e)} style={{backgroundColor:"#272B33"}}>
+            <Carousel.Item style={{width:"70%", margin:"auto"}}>
+              <div style={{float:"left"}}>
+                <img width={200} height={200} alt="200x200" src="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr02/2013/7/31/12/grid-cell-12788-1375287846-44.jpg"/>
+                <h3>Chapter Title</h3>
+              </div>
+              <div style={{float:"left"}}>
+                <img width={200} height={200} alt="200x200" src="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr02/2013/7/31/12/grid-cell-12788-1375287846-44.jpg"/>
+                <h3>Chapter Title</h3>
+              </div>
+              <div style={{float:"left"}}>
+                <img width={200} height={200} alt="200x200" src="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr02/2013/7/31/12/grid-cell-12788-1375287846-44.jpg"/>
+                <h3>Chapter Title</h3>
+              </div>
+              <div style={{float:"left"}}>
+                <img width={200} height={200} alt="200x200" src="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr02/2013/7/31/12/grid-cell-12788-1375287846-44.jpg"/>
+                <h3>Chapter Title</h3>
+              </div>
             </Carousel.Item>
         </Carousel>
         <Header showSearch hasUser currentUser={this.props.currentUser}/>
-        <h2 style={{marginTop:"80px", marginLeft:"40px"}}> Welcome, {this.props.currentUser} </h2>
       </div>
     );
   }
