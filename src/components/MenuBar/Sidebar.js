e120c273a1964fc39b42dd5b655e7ffa37f1ef89
jkvlevin <jake.k.levin@gmail.com>
2016-12-12 20:09:09 -0500

    server fixed

--- src/components/MenuBar/Sidebar.js
+++ src/components/MenuBar/Sidebar.js
@@ -13,7 +13,7 @@ import styles from './styles.css'
 const Sidebar = ({ isProf, handleCoursesClick, handleBrowseClick, handleSearchClick, handleSettingsClick, userName }) => {
   return (
     <div id="sidebar-container">
-      <h2 style={{color:"#807F83", textAlign:"center", marginBottom:"45px", fontSize:"20px"}}> <div id="logo"><Logo /></div> <br/> BookBag </h2>
+      <h2 style={{color:"#807F83", textAlign:"center", marginBottom:"35px", fontSize:"20px"}}> <div id="logo"><Logo /></div> <br/> BookBag </h2>
       <div id="sidebar-menu">
         {isProf ? <div> <Button id="menu-button" onClick={handleCoursesClick}><ToolsIcon id="menu-icon"/></Button> <h3 id="menu-title">Workbench</h3> <br/><br/><br/> </div>:
          <div><Button id="menu-button" onClick={handleCoursesClick}><LibraryIcon id="menu-icon"/></Button> <h3 id="menu-title">My Library</h3> <br/><br/><br/> </div>}
--- src/components/StudentLibrary/CourseDisplay.js
+++ src/components/StudentLibrary/CourseDisplay.js
@@ -9,7 +9,7 @@ const CourseDisplay = ({ courseName, chapters }) => {
       <div id="course-box" style={{width:"100%", height:"100%"}}>
         <ListGroup id="chapter-list" style={{marginLeft:"30px", marginTop:"30px", overflowY:"scroll"}}>
           {chapters.map(chapter =>
-            <ChapterList key={chapter.id} name={chapter.name} owner={chapter.owner} id={chapter.id} />
+            <ChapterList key={chapter.id} name={chapter.name} owner={chapter.owner} id={chapter.id} pdfUrl={chapter.pdf_url} />
           )}
         </ListGroup>
         <h1 style={{marginLeft:"25px", fontSize:"18px", color:"#807F83"}}> Course Notes </h1>
