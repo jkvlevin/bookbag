1e27d17980cb978b04702cc3471efb4ffdc3518c
jkvlevin <jake.k.levin@gmail.com>
2017-01-09 15:03:49 -0500

    first run through all works

--- src/components/StudentLibrary/FolderDisplay.js
+++ src/components/StudentLibrary/FolderDisplay.js
@@ -4,7 +4,7 @@ import ChapterList from './ChapterList';
 
 const FolderDisplay = ({ folderName, description, chapters, folders, addToFolder}) => {
   return (
-    <div style={{width:"90%", marginTop:"20px"}}>
+    <div style={{marginTop:"20px", width:"95%", height:"95%"}}>
       <h1 style={{marginLeft:"25px", fontSize:"24px", color:"#807F83"}}> {folderName} </h1>
       <p style={{marginLeft:"25px", fontSize:"13px", color:"#878787"}}>{description}</p>
       <div id="course-box" style={{width:"100%", height:"100%"}}>
